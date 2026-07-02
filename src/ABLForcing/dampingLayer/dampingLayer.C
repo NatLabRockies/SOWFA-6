032351920ddb23cc240abf25ce73f766d907d989
Matt Churchfield <mchurchf@kl1.head.cm.kestrel.hpc.nlr.gov>
2026-07-02 09:59:23 -0600

    Replaced the reference value with a Function1 type so that reference value can be uniform or height varying in a table.

--- src/ABLForcing/dampingLayer/dampingLayer.C
+++ src/ABLForcing/dampingLayer/dampingLayer.C
@@ -111,7 +111,6 @@ void Foam::dampingLayer<Type>::readSubDict()
 
         blendingFraction_[m] = subSubDict.lookupOrDefault<scalar>("blendingFraction",1.0);
 
-      //referenceValue_[m] = subSubDict.lookupOrDefault<Type>("referenceValue", Zero);
         referenceValue_[m] = Function1<Type>::New("referenceValue",subSubDict);
 
         dampedComponents_[m] = subSubDict.lookupOrDefault<Type>("dampedComponents", Zero);
@@ -413,7 +412,7 @@ void Foam::dampingLayer<Type>::update()
             }
 
           //Type diff = referenceValue_[m] - field_[cellID];
-            Type diff = referenceValue_[m]->value(z) - field_[cellID];
+            Type diff = referenceValue_[m]->value(meshPoint.z()) - field_[cellID];
 
             // The final source is s = (1/tau) * strength * componentMask * (u_ref - u)
             Type source = (1.0 / dampingTimeScale_[m]) * dampingStrength_[m][j] * cmptMultiply(diff,dampedComponents_[m]);
